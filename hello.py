2e6a146ec496109a791a8609a1486ee809018859
wandalao <1842915030@qq.com>
2019-11-14 15:03:13 +0800

    第三次提交

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World1'
+
+def writePid():
+    pid = str(os.getpid())
+    f = open('athena.pid', 'w')
+    f.write(pid)
+    f.close()
 if __name__ == '__main__':
     app.debug = True
+    writePid()
     app.run(host='0.0.0.0',debug=True)
