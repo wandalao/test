639d9123441571396c292d90d64daae7a7f75cf7
wandalao <1842915030@qq.com>
2019-11-14 10:58:57 +0800

    第二次修改

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Hello World'
+    return 'Hello World1'
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0',debug=True)
